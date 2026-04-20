11a27b5674363ac3d08ce50c4f6871ac0a0df6e8
Jorge-Enrique76 <26498814-Jorge-Enrique76@users.noreply.replit.com>
2026-04-20 17:30:48 +0000

    Restored to 'e1478ab745bb922d08d3cfa0e16cc8627ecf7723'
    Replit-Restored-To: e1478ab745bb922d08d3cfa0e16cc8627ecf7723

--- demo/src/main/java/com/example/demo/controller/MenuController.java
+++ demo/src/main/java/com/example/demo/controller/MenuController.java
@@ -2,7 +2,6 @@ package com.example.demo.controller;
 
 import com.example.demo.entitys.Comida;
 import com.example.demo.entitys.Categoria; // Importación explícita para evitar el "missing type"
-import com.example.demo.repository.AdicionalRepository;
 import com.example.demo.service.CategoriaService;
 import com.example.demo.service.ComidaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,9 +24,6 @@ public class MenuController {
     @Autowired
     CategoriaService categoriaService;
 
-    @Autowired
-    AdicionalRepository adicionalRepository;
-
     // http://localhost:5000/producto/menu
     @GetMapping("/menu")
     public String mostrarMenu(Model model) {
@@ -41,8 +37,6 @@ public class MenuController {
     public String verProducto(@PathVariable Long id, Model model) {
         Comida comida = comidaService.findById(id);
         model.addAttribute("comida", comida);
-        model.addAttribute("adicionalesCategoria",
-                adicionalRepository.findByCategorias_IdAndAvailableTrue(comida.getCategory().getId()));
         model.addAttribute("recomendaciones", comidaService.recomendadosPorCategoria(comida.getCategory().getId(), id));
         return "product-detail";
     }
