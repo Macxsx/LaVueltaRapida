bf78423356f65f4292fd35b438efd0be8d0567a9
Jeol28 <jorgeolaya2803@gmail.com>
2026-05-03 20:55:34 -0500

    Pasarela de pagos

--- demo/src/main/java/com/example/demo/config/CorsConfig.java
+++ demo/src/main/java/com/example/demo/config/CorsConfig.java
@@ -11,8 +11,11 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns(
+                        "http://localhost:4200",
+                        "http://127.0.0.1:4200",
                         "http://localhost:5000",
                         "http://127.0.0.1:5000",
+                        "https://hg6k75cd-4200.use2.devtunnels.ms",
                         "https://*.replit.dev",
                         "https://*.repl.co",
                         "https://*.devtunnels.ms"
